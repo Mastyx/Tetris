a979f66c2477447fca7c68c58e6ca4dbc0b7d8b0
mastyx <mastyx.house@gmail.com>
2022-08-04 19:11:36 +0200

    Prima Versione Completa

--- tetris.py
+++ tetris.py
@@ -90,7 +90,7 @@ class Tetris:
     
     def go_space(self):
         while not self.intersects():
-            sefl.figure.y += 1
+            self.figure.y += 1
         self.figure.y -= 1 
         self.freeze()
    
@@ -159,13 +159,54 @@ while not done:
                 game.rotate()
             if event.key == pygame.K_DOWN:
                 pressing_down = True
-                
+            if event.key == pygame.K_LEFT:
+                game.go_side(-1)
+                print("Sinistra")
+            if event.key == pygame.K_RIGHT:
+                game.go_side(1)
+                print("Destra")
+            if event.key == pygame.K_SPACE:
+                game.go_space()
+            if event.key == pygame.K_ESCAPE:
+                game.__init__(20,10)
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_DOWN:
+            pressing_down = False
+
+    screen.fill(WHITE)
+    # disegnamo la griglia
+    for i in range(game.height):
+        for j in range(game.width):
+            pygame.draw.rect(screen, GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
+            if game.field[i][j] > 0:
+                pygame.draw.rect(screen, colori_figure[game.field[i][j]],
+                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
+
+    
+    if game.figure is not None:
+        for i in range(4):
+            for j in range(4):
+                p = i * 4 + j
+                if p in game.figure.image():
+                    pygame.draw.rect(screen, colori_figure[game.figure.color],
+                                     [game.x + game.zoom * (j + game.figure.x) + 1,
+                                      game.y + game.zoom * (i + game.figure.y) + 1,
+                                      game.zoom - 2, game.zoom - 2])
+    
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    font1 = pygame.font.SysFont('Calibri', 45, True, False)
+    text = font.render("Score: " + str(game.score), True, BLACK)
+    text_game_over = font1.render("Game Over", True, (255, 125, 0))
+    text_game_over1 = font1.render("Press ESC", True, (255, 215, 0))
+    screen.blit(text, [0, 0])
+
+    if game.state == "gameover":
+        screen.blit(text_game_over, [60, 200])
+        screen.blit(text_game_over1, [65, 265])
 
     pygame.display.flip()
     clock.tick(fps)
-    screen.fill(WHITE)
-    font = pygame.font.SysFont('Calibri', 25, True, False)
-    text = font.render("Score : ", True, BLACK)
-    screen.blit(text, [0, 0])
+    
+    
 pygame.quit()
 
